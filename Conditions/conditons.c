6beec91624ac2127e02cd653d1dedf1976a9e4e8
Elchin Guliyev <elcnqlyv24@gmail.com>
2026-09-09 14:30:40 +0400

    Refactor code structure for improved readability and maintainability

--- Conditions/conditons.c
+++ Conditions/conditons.c
@@ -6,7 +6,7 @@ void guessNumber(int guess) {
     } else if (guess > 555) {
         printf("Your guess is too high.\n");
     } else {
-        printf("Congratulations! You guessed the correct number.\n");
+        printf("Correct. You guessed it!\n");
     }
 }
 
