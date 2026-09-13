abb45913bcdfbeb37479503fef63214fa348eb01
Elchin Guliyev <elcnqlyv24@gmail.com>
2026-09-13 10:26:52 +0400

    Refactor 3.1-logic.c to implement balance check and add 3.2-loops.c as a new file

--- C-Crash/3.1-logic/3.1-logic.c
+++ C-Crash/3.1-logic/3.1-logic.c
@@ -4,9 +4,12 @@
 int main()
 
 {
-    int age = 17;
-    if (age < 150) printf ("You might be alive \n"); 
+    double balance = -5000;
+    balance > 0 ? printf("You have money \n") : printf("No money yo \n");
 
+    bool hasMoney;
+    hasMoney = balance > 0 ? 1 : 0;
 
-    return 0;
+    printf("Has money? 1 is yes 0 is no: %d\n", hasMoney);
+
 }
